e4208ff18e100272390ee7b00202b8ef961fef1f
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-06 06:21:58 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,5 +46,5 @@ dependencies {
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.android.gms:play-services-auth:21.0.0")
 
-    implementation("com.github.barteksc:AndroidPdfViewer:2.8.2")
+    implementation("com.github.mhiew:android-pdf-viewer:3.2.0-1")
 }
